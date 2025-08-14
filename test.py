ea5337f88ce746425cd1351f615bd136f33dcd76
marslan199 <mianarslan19900@gmail.com>
2025-08-14 07:21:12 -0700

    Adding_Colum_to_Calculate_Spa,_Score

--- test.py
+++ test.py
@@ -1,9 +1,11 @@
 import pandas as pd
 
-# Load the dataset (change file name if needed)
+# Load the dataset
 df = pd.read_csv("emails.csv")
+df.columns = df.columns.str.lower()
+words = ["money" , "free" , "win" ,  "urgent" , "act now", "limited time", "click here","Guaranteed" , "Be your own boss" , "Billion" , "Free gift" , "Giveaway" , "Miracle", "Please read" , "Winner" ]
 
-# Peek at the first few rows
-print(df.head())
-print(df.info())
-print(df.isnull().sum())
+spam_words = [w.lower() for w in words if w in df.columns]
+df["spam_Score"] = df[spam_words].sum(axis=1)
+
+print(df[["spam_Score"]].tail())
